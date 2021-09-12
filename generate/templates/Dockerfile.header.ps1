ad56f837a8bcdcdf020605c2ca94c8b7105317fd
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2021-09-12 09:31:49 +0000

    Enhancement: Include gnupg in all sops variants

--- generate/templates/Dockerfile.header.ps1
+++ generate/templates/Dockerfile.header.ps1
@@ -20,6 +20,8 @@ RUN apk add --no-cache libressl
 
 
 RUN wget -qO- https://github.com/mozilla/sops/releases/download/v3.7.1/sops-v3.7.1.linux > /usr/local/bin/sops && chmod +x /usr/local/bin/sops
+
+RUN apk add --no-cache gnupg
 "@
 }
 
