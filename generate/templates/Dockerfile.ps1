3e85a31ea0aefb13aa01ead609ed5eeb67676a29
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2023-09-20 13:20:31 +0000

    Enhancement: Change to `ansible` community package for `ansible-core >= 2.10` variants

--- generate/templates/Dockerfile.ps1
+++ generate/templates/Dockerfile.ps1
@@ -7,15 +7,6 @@ RUN echo "I am running on `$BUILDPLATFORM, building for `$TARGETPLATFORM"
 # Install ansible
 RUN set -eux; \
     apk add --no-cache $( $VARIANT['_metadata']['package'] )$( if ( $VARIANT['_metadata']['distro'] -eq 'alpine' -and $VARIANT['_metadata']['distro_version'] -eq '3.6' ) { '=' } else { '~=' } )$( $VARIANT['_metadata']['package_version'] ); \
-
-"@
-$( if ($VARIANT['_metadata']['package'] -eq 'ansible-core') {
-@"
-    apk add ansible; \
-
-"@
-})
-@"
     ansible --version
 
 RUN apk add --no-cache ca-certificates
